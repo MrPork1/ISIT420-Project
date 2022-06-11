76054dfa24de0bc077ae9a7d9ac882a06ef47891
Sean <seankel@comcast.net>
2022-06-10 18:18:42 -0700

    Added query 1 and part of query 2

--- Controllers/QueryController.cs
+++ Controllers/QueryController.cs
@@ -1,5 +1,7 @@
 ﻿using ISIT420_Project.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Diagnostics;
 
 namespace ISIT420_Project.Controllers
 {
@@ -8,37 +10,123 @@ namespace ISIT420_Project.Controllers
     [Produces("application/json")]
     public class QueryController : Controller
     {
-        // GET: QueryController
-        public ActionResult Index()
+        [HttpGet]
+        public List<ParticipationData> Get()
         {
-            return View();
-        }
+            var context = new Models.SocialDBContext();
+            var mainQuery = from each in context.ParticipationTables
+                        select each;
+            var result = mainQuery.ToList();
 
-        // GET: QueryController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+            //var newQuery = from each in result
+            //               group each by each.Region;
 
-        // GET: QueryController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+            //foreach (var item in newQuery)
+            //{
+            //    Debug.WriteLine(item.Key);
+            //    //double newAverage = 0.0;
+            //    //int count = 0;
+            //    foreach (var p in item)
+            //    {
+            //        //count++;
+            //        //newAverage += (double)p.Percentage;
+            //        Debug.WriteLine(p.Percentage);
+            //    }
+            //    //double average = newAverage / count;
+            //}
 
-        // POST: QueryController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
+
+            var averageByRegion = result.GroupBy(r => r.Region, p => p.Percentage).Select(r => new
             {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
+                Region = r.Key,
+                Percentage = r.Average()
+            });
+
+
+            //foreach (var p in averageByRegion)
+            //{
+            //    Debug.WriteLine(p.Region + " " + p.Percentage);
+            //}
+
+            var secondQuery = from each in context.FriendsTables
+                              where each.FrequencyId == 2 || each.FrequencyId == 5 || each.FrequencyId == 6
+                              select each;
+
+            var result2 = secondQuery.ToList();
+
+            var averageByFrequency = secondQuery.GroupBy(r => r.Region, p => p.Percentage).Select(r => new
             {
-                return View();
+                Region = r.Key,
+                Percentage = r.Average()
+            });
+
+            var thirdQuery = from each in context.FriendsTables
+                              where each.FrequencyId == 1 || each.FrequencyId == 3 || each.FrequencyId == 4 || each.FrequencyId == 7
+                              select each;
+
+            var averageByLessFrequencies = thirdQuery.GroupBy(r => r.Region, p => p.Percentage).Select(r => new
+            {
+                Region = r.Key,
+                Percentage = r.Average()
+            });
+
+            //foreach (var p in averageByFrequency)
+            //{
+            //    Debug.WriteLine(p.Region + " " + p.Percentage);
+            //}
+
+            //var innerJoinQuery = from avgRegion in averageByRegion
+            //                     join per in averageByFrequency on avgRegion.Region equals per.Region
+            //                     join less in averageByLessFrequencies on per.Region equals less.Region
+            //                     select new ParticipationData
+            //                     {
+            //                         Region = avgRegion.Region,
+            //                         Percentage1 = (double)avgRegion.Percentage,
+            //                         Percentage2 = (double)per.Percentage,
+            //                         Percentage3 = (double)less.Percentage
+            //                     };
+
+            //foreach (var p in innerJoinQuery)
+            //{
+            //    Debug.WriteLine(p.Region + " " + p.Percentage1 + " " + p.Percentage2 + " " + p.Percentage3);
+            //}
+
+            //var newList = innerJoinQuery.ToList();
+            //return newList;
+
+            var innerJoinQuery = from avgRegion in averageByRegion
+                                 join per in averageByFrequency on avgRegion.Region equals per.Region
+                                 join less in averageByLessFrequencies on per.Region equals less.Region
+                                 select new ParticipationData
+                                 {
+                                     Region = avgRegion.Region,
+                                     Percentage1 = (double)(per.Percentage/100)*((double)avgRegion.Percentage),
+                                     Percentage2 = (double)(less.Percentage/100)*(double)avgRegion.Percentage
+                                 };
+
+            foreach (var p in innerJoinQuery)
+            {
+                Debug.WriteLine(p.Region + " " + p.Percentage1 + " " + p.Percentage2 + " " + p.Percentage3);
             }
+
+            var newList = innerJoinQuery.ToList();
+            return newList;
+
+
+            //
+            //return averageByRegion.ToList();
+
+
+            //var query = (from eachCountEvent in context.ParticipationTables
+            //             where eachCountEvent.FrequencyId == 7
+            //             group eachCountEvent by eachCountEvent.Region, eachCountEvent.Percentage into i
+            //             select new ParticipationData
+            //             {
+            //                 Region = i.Key,
+            //             });
+
+            //var result1 = query.OrderByDescending(a => a.Percentage);
+            //return query.ToList();
         }
 
         // GET: QueryController/Edit/5
@@ -47,40 +135,18 @@ namespace ISIT420_Project.Controllers
             return View();
         }
 
-        // POST: QueryController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
         // GET: QueryController/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
+    }
 
-        // POST: QueryController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+    public class ParticipationData
+    {
+        public string Region { get; set; }
+        public double Percentage1 { get; set; }
+        public double Percentage2 { get; set; }
+        public double Percentage3 { get; set; }
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,31 +1,66 @@
-﻿$(document).ready(function () {
+﻿//$(document).ready(function () {
 
 
-    $.getJSON('api/Frequency')
+//    $.getJSON('api/Frequency')
+//        .done(function (data) {
+//            let dataArray = data;
+//            let selectID = document.getElementById("frequencies");
+//            dataArray.forEach(function (value) {
+//                selectID.appendChild(new Option(value.frequency, value.frequencyId));
+//            });
+
+//        });
+
+//    $.getJSON('api/Gender')
+//        .done(function (data) {
+//            let dataArray = data;
+//            let selectID = document.getElementById("genders");
+//            dataArray.forEach(function (value) {
+//                selectID.appendChild(new Option(value.gender, value.genderId));
+//            });
+//        });
+
+//    $.getJSON('api/Age')
+//        .done(function (data) {
+//            let dataArray = data;
+//            let selectID = document.getElementById("ages");
+//            dataArray.forEach(function (value) {
+//                selectID.appendChild(new Option(value.ageRange, value.ageId));
+//            });
+//        });
+//});
+
+function participationQuery() {
+    $.getJSON('api/Query')
         .done(function (data) {
-            let dataArray = data;
-            let selectID = document.getElementById("frequencies");
-            dataArray.forEach(function (value) {
-                selectID.appendChild(new Option(value.frequency, value.frequencyId));
-            });
+            console.log(data);
+            var table = document.getElementById('query1Table');
+            // creating rows
+            for (var r = 0; r < data.length; r++) {
+                var row = document.createElement("tr");
 
-        });
+                var cell0 = document.createElement("td");
+                var cell1 = document.createElement("td");
+                var cell2 = document.createElement("td");
+               // var cell3 = document.createElement("td");
 
-    $.getJSON('api/Gender')
+                cell0.appendChild(document.createTextNode(data[r].region));
+                row.appendChild(cell0);
+                cell1.appendChild(document.createTextNode(data[r].percentage1));
+                row.appendChild(cell1);
+                cell2.appendChild(document.createTextNode(data[r].percentage2));
+                row.appendChild(cell2);
+              //  cell3.appendChild(document.createTextNode(data[r].percentage3));
+              //  row.appendChild(cell3);
+
+                table.appendChild(row); // add the row to the end of the table body
+            }
+        })
+}
+
+function query2Function() {
+    $.getJSON('api/Query2')
         .done(function (data) {
-            let dataArray = data;
-            let selectID = document.getElementById("genders");
-            dataArray.forEach(function (value) {
-                selectID.appendChild(new Option(value.gender, value.genderId));
-            });
-        });
-
-    $.getJSON('api/Age')
-        .done(function (data) {
-            let dataArray = data;
-            let selectID = document.getElementById("ages");
-            dataArray.forEach(function (value) {
-                selectID.appendChild(new Option(value.ageRange, value.ageId));
-            });
-        });
-});
+            console.log(data);
+        })
+}
